5363529b7671d07a448cf070646ab9cc5e9fecf9
utof0x <popovychmykhailo00@gmail.com>
2023-07-16 14:07:38 +0300

    added GET method to get post with specific id

--- config/Database.php
+++ config/Database.php
@@ -9,13 +9,12 @@ class Database
     private string $password = '';
     private $conn;
 
-    public function connect()
+    public function connect(): null|PDO
     {
         $this->conn = null;
 
         try {
             $dsn = 'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname;
-//            mysql:host=127.0.0.1;port=3306;dbname=lightHp
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
--- models/Post.php
+++ models/Post.php
@@ -5,12 +5,12 @@ class Post {
     private string $table = 'posts';
 
     public string $id;
-    public string $category_id;
-    public string $category_name;
-    public string $title;
-    public string $body;
-    public string $author;
-    public string $created_at;
+    public string $category_id = '';
+    public string $category_name = '';
+    public string $title = '';
+    public string $body = '';
+    public string $author = '';
+    public string $created_at = '';
 
     public function __construct($db)
     {
@@ -37,5 +37,35 @@ class Post {
         $statement = $this->conn->prepare($sql_query);
         $statement->execute();
         return $statement;
+    }
+
+    public function read_single()
+    {
+        $sql_query = 'SELECT
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+            FROM
+            ' . $this->table . ' p
+            LEFT JOIN
+                categories c ON p.category_id = c.id
+            WHERE
+                p.id = :id';
+
+        $statement = $this->conn->prepare($sql_query);
+        $statement->bindValue(':id', $this->id);
+        $statement->execute();
+
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
     }
 }
